e03562204027def66f4580a09d95a4d9c0466081
WillDuss <no_name3344@hotmail.com>
2022-12-02 01:16:48 -0500

    initial Day2 commit

---
+++ Day2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            input = File.Open("Day2/input.txt");
+        }
+    }
+}
