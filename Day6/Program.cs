d356d5d7d963a8a5fc8274a1340ffea66a73ac78
WillDuss <no_name3344@hotmail.com>
2022-12-06 14:30:59 -0500

    Day6 Initial Solve

---
+++ Day6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Day6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText(@"input.txt");
+            List<char> buffer = new List<char>(4);
+            int index = 0;
+
+            for(int i = 0; i < input.Length && index == 0; i++){
+                buffer.Add(input[i]);
+
+                if (buffer.Count == 14){
+                    if (buffer.Distinct().Count() == 14) index = i + 1;
+                    buffer.Clear();
+                    i -= 13;
+                }
+            }
+            Console.WriteLine(index);
+
+        }
+    }
+}
